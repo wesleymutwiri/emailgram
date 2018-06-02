74450130fcf6c2cf5fcaf18d06378baf241801ef
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-03 02:27:55 +0300

    created components for auth and chats

--- chatire-frontend/src/router/index.js
+++ chatire-frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Chat from '@/components/Chat'
+import UserAuth from '@/components/UserAuth'
 
 Vue.use(Router)
 
