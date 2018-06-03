f1e01423c22ef34bd2a90d4a33f6f0839249e170
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-03 14:51:40 +0300

    created first views

--- chats/views.py
+++ chats/views.py
@@ -4,9 +4,23 @@ from rest_framework.response import Response
 from rest_framework import permissions
 from .models import ChatSession, ChatSessionMember, ChatSessionMessage, deserialize_user
 
+# views 
+
 class ChatSessionView(APIView):
     '''
     Manage Chat sessions 
     '''
     permission_classes = (permissions.IsAuthenticated,)
-    
+
+    def post(self, request, *args, **kwargs):
+        '''
+        Create a new chat session
+        '''
+        user = request.user 
+        chat_session = ChatSession.objects.create(owner=user)
+
+        return Response({
+            'status': 'SUCCESS','uri':chat_session.uri,
+            'message': 'New chat session created'
+        })
+    def 
