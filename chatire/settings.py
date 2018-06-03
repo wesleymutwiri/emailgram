ee03ac0ad01db210e066abc2f345762216d6b9e5
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-03 08:15:16 +0300

    addedcorsheaders to the settings

--- chatire/settings.py
+++ chatire/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_framework',
     'chats',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -52,7 +53,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL=True
 
 ROOT_URLCONF = 'chatire.urls'
 
