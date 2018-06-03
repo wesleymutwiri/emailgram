031a4b2cc1ae1cca76ef316869dc8d3b4f712c13
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-03 14:49:15 +0300

    creating views

--- chats/views.py
+++ chats/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response 
+from rest_framework import permissions
+from .models import ChatSession, ChatSessionMember, ChatSessionMessage, deserialize_user
 
-# Create your views here.
+class ChatSessionView(APIView):
+    '''
+    Manage Chat sessions 
+    '''
+    permission_classes = (permissions.IsAuthenticated,)
+    
