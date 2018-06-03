d247b5bbbccabeb8fd37a104acfb257675e18489
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-03 14:14:24 +0300

    new classes

--- chats/models.py
+++ chats/models.py
@@ -21,4 +21,6 @@ class TrackableDateModel(models.Model):
     '''
     create_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
+
+    class 
     
