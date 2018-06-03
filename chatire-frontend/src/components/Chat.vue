9f426bc12055e049ec0b7d58d32011121166ec3a
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-03 20:36:11 +0300

    new vue js template

---
+++ chatire-frontend/src/components/Chat.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 offset-3">
+                <div v-if="sessionStarted" class="card" id="chat-container">
+                    <div class="card-header text-white text-center font-weight-bold subtle-blue-gradient">
+                        Share the page URL to invite new friends
+                    </div>
+                    <div class="card-body">
+                        <div class="container chat-body">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
