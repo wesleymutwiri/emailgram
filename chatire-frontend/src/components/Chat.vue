55927e10163450c84065d58c1580524d0fba5039
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-03 20:46:09 +0300

    new vue js template

--- chatire-frontend/src/components/Chat.vue
+++ chatire-frontend/src/components/Chat.vue
@@ -8,7 +8,24 @@
                     </div>
                     <div class="card-body">
                         <div class="container chat-body">
+                            <div class="row chat-section">
+                                <div class="col-sm-2">
+                                    <img src="http://placehold.it/40/f16000/fff&text=D" alt="" class="rounded-circle">
+                                </div>
+                                <div class="col-sm-7">
+                                    <span class="card-text speech-bubble speech-bubble-peer">Hello!</span>
+                                </div>
+                            </div>
+                            <div class="row chat-section">
+                                <div class="col-sm-7 offset-3">
+                                    <span class="card-text speech-bubble speech-bubble-user float-right text-white subtle-blue-gradient">Whatsup, another chat app?
 
+                                    </span>
+                                </div>
+                                <div class="col-sm-2">
+                                    <img src="http://placehold.it/40/f16000/fff&text=D" alt="" class="rounded-circle">
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
