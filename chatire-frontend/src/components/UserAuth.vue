5eaedc69883d90009bbf3b5f651e957f7228ce25
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-03 03:41:50 +0300

    changes to the frontend

--- chatire-frontend/src/components/UserAuth.vue
+++ chatire-frontend/src/components/UserAuth.vue
@@ -64,6 +64,30 @@ export default {
             }
         }
     }
+methods: {
+    signUp () {
+        $.post('http://localhost:8000/auth/users/create/', this.$data, (data)=>{
+            alert("Your account has been created. You will be signed in automatically")
+            this.signIn()
+        })
+        .fail((response) => {
+            alert(response.responseText)
+        })
+    },
+
+    signIn () {
+        const credentials = {username: this.username, password: this.password}
+
+        $.post('http://localhost:8000/auth/token/create/', credentials, (data) =>{
+            sessionStorage.setItem('authToken', data.auth_token)
+            sessionStorage.setItem('username', this.username)
+            this.$router.push('.chats')
+        })
+        .fail((response) => {
+            alert(response.responseText)
+        })
+    }
+}
 </script>
 <style scoped>
 #auth-container{
