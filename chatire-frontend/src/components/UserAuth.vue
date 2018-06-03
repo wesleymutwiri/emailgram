08ae682b75f91ab9240ec226caea8bb6760be566
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-03 03:47:15 +0300

    added new requirement

--- chatire-frontend/src/components/UserAuth.vue
+++ chatire-frontend/src/components/UserAuth.vue
@@ -65,8 +65,8 @@ export default {
         }
     }
 methods: {
-    signUp () {
-        $.post('http://localhost:8000/auth/users/create/', this.$data, (data)=>{
+    signUp() {
+        $.post('http://localhost:8000/auth/users/create/', this.$data, (data) => {
             alert("Your account has been created. You will be signed in automatically")
             this.signIn()
         })
