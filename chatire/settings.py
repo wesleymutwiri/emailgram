53b6842d5071e728d4bfe9babbb2b8bd51d81351
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-03 08:16:08 +0300

    configuring for pushing to heroku

--- chatire/settings.py
+++ chatire/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fd^-%s&^z=!p7hpyenq0qzbt#jj#_p_oc*x$9nnel7)+pwe+pi'
+SECRET_KEY=config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG=config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
