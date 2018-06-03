0c22b421bad6862ba52e7d6c6563883e2a14e239
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-03 19:21:33 +0300

    added urls of chats to main application

--- chatire/urls.py
+++ chatire/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
+    path('api/', include('chats.urls')),
 ]
--- chats/urls.py
+++ chats/urls.py
@@ -3,8 +3,12 @@ from . import views
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import path
 
 urlpatterns = [
     url(r'^$', views.index, name= 'index'),
+    path('chats/', views.ChatSession.as_view()),
+    path('chats/<url>/', views.ChatSession.as_view()),
+    path('chats/<url>/messsages/', views.ChatSessionMessageView.as_view()),
 ]
 
